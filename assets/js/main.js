690e04d43d88a66dcc755f9f357aed92e43649da
Omereshone Kelvin Oghenerhoro <kelvinomereshone@gmail.com>
2019-01-21 20:51:30 +0100

    added scroll reveal

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+ScrollReveal({ reset: true, duration: 1000 });
+ScrollReveal().reveal('.objective');
+ScrollReveal().reveal('#services', {delay: 2000});
+ScrollReveal().reveal('#values', {delay: 3000});
+ScrollReveal().reveal('#contact', {delay: 4000});
